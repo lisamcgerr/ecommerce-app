8c7168fc31360b383f492e3a096e37a9f9f50651
Lisa McGerr <lisa.mcgerr@borngroup.com>
2022-10-26 10:43:01 -0500

    barebones for Home.css & Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import '../style/Home.css';
 
 const Home = () => {
 
     return (
-        <div>
-            Home
+        <div className="home">
+            <div className="home__container">
+            <img
+                className="home__image"
+                src="https://user-images.githubusercontent.com/73184313/127373370-a925e758-eba5-461f-834e-2fe14498a6e1.jpg"
+                alt="grey paint background" />
+            </div>
         </div>
     )
 }
