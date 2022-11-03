7792986cc6f97e4543609ad942d73a707d248b31
lisamcgerr <lisamarie.mcgerr@gmail.com>
2022-11-03 13:41:48 -0500

    subtotal component and css file added and currency format installed

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -2,36 +2,45 @@ import React from 'react';
 import '../style/Checkout.css'
 import { useStateValue } from '../contexts/StateProvider';
 import CheckoutProduct from './CheckoutProduct';
+import Subtotal from './Subtotal';
+
 
 const Checkout = () => {
     const [{basket}] = useStateValue();
 
     return (
         <div className="checkout">
+            <div className="checkout__left">
             {/* @TODO replace image created by... */}
-            <img
-                className="checkout__ad"
-                src="https://user-images.githubusercontent.com/73184313/131029015-fa2d4214-8a04-4480-b9c7-67a3ceed3384.png"
-                alt="grey textured background that reads created by Lisa McGerr Full Stack Web Developer"
-            />
-            {basket?.length === 0 ? (
-                <div>
-                    <h2 className="checkout__title">Your Shopping Basket is Empty</h2>
-                </div>
-            ) : (
-                <div>
-                    <h2 className="checkout__title">Your Shopping Basket</h2>
-                    {/* @TODO render all checkout Products HERE */}
-                    {basket?.map(item => ( 
-                        <CheckoutProduct
-                            id={item.id}
-                            title={item.title}
-                            image={item.image}
-                            price={item.price}
-                            rating={item.rating}
-                        />
-                    ))}
-                </div>
+                <img
+                    className="checkout__ad"
+                    src="https://user-images.githubusercontent.com/73184313/131029015-fa2d4214-8a04-4480-b9c7-67a3ceed3384.png"
+                    alt="grey textured background that reads created by Lisa McGerr Full Stack Web Developer"
+                />
+                {basket?.length === 0 ? (
+                    <div>
+                        <h2 className="checkout__title">Your Shopping Basket is Empty</h2>
+                    </div>
+                ) : (
+                    <div>
+                        <h2 className="checkout__title">Your Shopping Basket</h2>
+                        {/* @TODO render all checkout Products HERE */}
+                        {basket?.map(item => (
+                            <CheckoutProduct
+                                id={item.id}
+                                title={item.title}
+                                image={item.image}
+                                price={item.price}
+                                rating={item.rating}
+                            />
+                        ))}
+                    </div>
+                )}`
+            </div>
+            {basket?.length > 0 && (
+            <div className="checkout__right">
+                <Subtotal />
+            </div>
             )}
         </div>
     )
